6b6e449fb808227b87cbc5f0c5e6c5138fde07e9
amnanjum01 <64750577+amnanjum01@users.noreply.github.com>
2024-03-20 23:23:13 +0500

    Class identification
    Identification of list of classes generated by the model (in numeric codes)

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ from flask import Flask, request, jsonify
 from io import BytesIO
 from PIL import Image
 from ultralytics import YOLO
-
-
+import numpy as np
+from classificationClasses.ClothClasses import ClothClasses
 app = Flask(__name__)
 predictionModel = YOLO(r'C:\Users\Ammna\Documents\GitHub\FYP-BACKEND\clothing_items.pt')
+#only one instance of cloth class
+clothClasses = ClothClasses()
 
 @app.route("/detect", methods=["POST"])
-def detect_clothing_item():
+def detectClothingItem():
     if request.method == 'POST':
         imageInput = request.files["image"]
         imageInput2 = Image.open(BytesIO(imageInput.read()))
-        prediction = predictionModel(imageInput2)
-        finalOutput = {"result": prediction[0].boxes.data}
-        print(finalOutput)
-        return "Hello!"
+        predictionResult = predictionModel(imageInput2)
+        inferences = predictionResult[0].boxes.data.numpy()
+        val = []
+        for inference in inferences:
+            val.append(inference[5])
+        retirevedClasses = {"results": clothClasses.getClassLabels(val)}
+        return jsonify(retirevedClasses)
         
---
+++ classificationClasses/ClothClasses.py
@@ -0,0 +1,26 @@
+class ClothClasses:
+    #class variables
+    classLabelsDict = {
+        0: "m-button-down",
+        1: "m-formal-trousers",
+        2: "m-informal-pants",
+        3: "m-kurta",
+        4: "m-shalwar",
+        5: "m-shirts",
+        6: "outerwear",
+        7: "w-dress",
+        8: "w-dupatta",
+        9: "w-eastern-trouser",
+        10: "w-kameez",
+        11: "w-shalwar",
+        12: "w-western-shirt",
+        13: "w-western-trouser"
+    }
+    
+    #class method to obtain the required data
+    @classmethod
+    def getClassLabels(cls, classCodes):
+        classNames = []
+        for classCode in classCodes:
+            classNames.append(cls.classLabelsDict[classCode])
+        return classNames
